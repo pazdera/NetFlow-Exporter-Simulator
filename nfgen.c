2602aebe67c6bfa45abc8a99c0f5f32c7c0e7e89
Radek Pazdera <radek.pazdera@gmail.com>
2010-11-24 10:03:08 +0100

    Hacking, hacking, but cant make it work.

--- nfgen.c
+++ nfgen.c
@@ -17,22 +17,26 @@
  * along with nfgen.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <stdint.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <netdb.h>
-#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
+
 #include <sys/stat.h>
+// #include <sys/types.h>
+
+
+#include <string.h>
 #include <time.h>
 
 #define TCP_PROTO 6
 #define UDP_PROTO 17
 
-#define MIN_DELAY 1
-#define MAX_DELAY 60
+#define MIN_FLOW_DURATION 1
+#define MAX_FLOW_DURATION 60
 
 #define SRC_PORT 10000
 #define DEST_PORT 2055
@@ -43,20 +47,44 @@
 #define NETFLOW_RECORD_SIZE 48
 
 #define NUMBER_OF_ADDRESSES 4
-const in_addr_t addresses[NUMBER_OF_ADDRESSES] =
+in_addr_t addresses[NUMBER_OF_ADDRESSES] =
 {
+//   "127.0.0.1",
+//   "192.168.1.100",
+//   "192.168.1.101",
+//   "192.168.1.102"
   0x7F000001, /* 127.0.0.1 */
   0xC0A80164, /* 192.168.1.100 */
   0xC0A80165, /* 192.168.1.101 */
   0xC0A80166  /* 192.168.1.102 */
 };
 
-in_addr_t pickRandomAddress()
+
+
+// in_addr_t convertAddress(char *addressInDotNotation)
+// {
+//   in_addr structureForConversionResult;
+// 
+//   if (inet_aton(addressInDotNotation, &structureForConversionResult) != 1)
+//   {
+//     perror("Address conversion failed.");
+//   }
+// 
+//   return structureForConversionResult.s_addr;
+// }
+
+in_addr_t generateRandomAddress()
 {
+  /* This could be more sophisticated */
   return addresses[(1 + rand()) % NUMBER_OF_ADDRESSES];
 }
 
-char randomTCPFlags()
+in_port_t generateRandomPortNumber()
+{
+  return rand() % 65536;
+}
+
+char generateRandomTCPFlags()
 {
   return rand() % 255;
 }
@@ -105,25 +133,24 @@ size_t makeNetflowPacket(char *buffer, int numberOfFlows, time_t systemStartTime
   for (int flow = 0;flow < numberOfFlows; flow++)
   {
     // Setup record
-    memset(&record, 0, sizeof(record));
-    record.srcAddr = htonl(pickRandomAddress());
-    record.dstAddr = htonl(pickRandomAddress());
+    record.srcAddr = htonl(generateRandomAddress());
+    record.dstAddr = htonl(generateRandomAddress());
     record.nextHop = 0;
     record.input = 0;
     record.output = 0;
     record.dPkts = htonl(100);
     record.dOctets = htonl(120*8);
 
-    record.first = (currentTime - systemStartTime - (MIN_DELAY + rand()) % MAX_DELAY)*1000;
-    record.last = record.first + (rand() % MAX_DELAY)*1000;
+    record.first = (currentTime - systemStartTime - (MIN_FLOW_DURATION + rand()) % MAX_FLOW_DURATION)*1000;
+    record.last = record.first + (rand() % MAX_FLOW_DURATION)*1000;
     record.first = htonl(record.first);
     record.last = htonl(record.last);
 
-    record.srcPort = htons(rand() % 65535);
-    record.dstPort = htons(rand() % 65535);
+    record.srcPort = htons(generateRandomPortNumber());
+    record.dstPort = htons(generateRandomPortNumber());
     record.pad = 0;
     record.prot = rand() % 2 ? TCP_PROTO : UDP_PROTO;
-    record.tcpFlags = record.prot == TCP_PROTO ? randomTCPFlags() : 0;
+    record.tcpFlags = record.prot == TCP_PROTO ? generateRandomTCPFlags() : 0;
     record.tos = 0;
     record.srcAs = 0;
     record.dstAs = 0;
@@ -135,8 +162,6 @@ size_t makeNetflowPacket(char *buffer, int numberOfFlows, time_t systemStartTime
   }
 
   // Setup header
-  memset(&header, 0, sizeof(header));
-
   header.version      = 5;
   header.count        = numberOfFlows;
   header.sysUpTime    = htonl((currentTime - systemStartTime) * 1000);
