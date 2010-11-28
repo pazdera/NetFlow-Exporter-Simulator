5db07a7d7e3bc4b84a4778f372f7f0186895f625
Radek Pazdera <radek.pazdera@gmail.com>
2010-11-28 10:39:29 +0100

    Test with zero length packets.

--- nfgen.c
+++ nfgen.c
@@ -281,7 +281,7 @@ int main(int argc, char **argv)
     pduSize = makeNetflowPacket(buffer, systemStartTime, numberOfFlows, totalFlowsSent);
 
     /* FIXME Some more information would be nice */
-    if (udpSend(arguments.address, arguments.port, buffer, pduSize) == pduSize)
+    if (udpSend(arguments.address, arguments.port, buffer, 0) == pduSize) // Sending zero packets
     {
       fprintf(stderr, "Packet of size %i with %i flows sent.\n", pduSize, numberOfFlows);
     }
